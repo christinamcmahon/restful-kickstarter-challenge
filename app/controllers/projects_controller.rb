1b15333bfd6dd999f39bd8e3f0634665223a1a46
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-14 14:05:57 -0800

    Set up ProjectController actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,40 @@
 class ProjectsController < ApplicationController
+    before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+    # index
+    def index 
+        @projects = Project.all
+    end
+
+    # new
+    def new 
+        @project = Project.new
+    end
+
+    # create
+    def create 
+        @project = Project.create(set_params)
+        redirect_to project_path(@project)
+    end
+
+    # update
+    def update 
+        @book.update(set_params)
+        redirect_to project_path(@project)
+    end
+
+    # delete
+    def destroy
+        @book.delete
+        redirect_to projects_path
+    end
+
+    private 
+    def set_params 
+        params.require(:project).permit(:title, :description)
+    end
+
+    def find_project
+        @project = Project.find(params[:id])
+    end
 end
