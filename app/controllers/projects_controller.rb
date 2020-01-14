44f46c7c238bc3dda5a37e0a5822dc785f0aede5
christinamcmahon <christina.mcmahon1@gmail.com>
2020-01-14 15:03:03 -0800

    Added delete and edit buttons

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,40 +1,41 @@
 class ProjectsController < ApplicationController
-    before_action :find_project, only: [:show, :edit, :update, :destroy]
+  before_action :find_project, only: [:edit, :update, :destroy, :show]
 
-    # index
-    def index 
-        @projects = Project.all
-    end
+  # index
+  def index
+    @projects = Project.all
+  end
 
-    # new
-    def new 
-        @project = Project.new
-    end
+  # new
+  def new
+    @project = Project.new
+  end
 
-    # create
-    def create 
-        @project = Project.create(set_params)
-        redirect_to project_path(@project)
-    end
+  # create
+  def create
+    @project = Project.create(set_params)
+    redirect_to project_path(@project)
+  end
 
-    # update
-    def update 
-        @book.update(set_params)
-        redirect_to project_path(@project)
-    end
+  # update
+  def update
+    @project.update(set_params)
+    redirect_to project_path(@project)
+  end
 
-    # delete
-    def destroy
-        @book.delete
-        redirect_to projects_path
-    end
+  # delete
+  def destroy
+    @project.delete
+    redirect_to projects_path
+  end
 
-    private 
-    def set_params 
-        params.require(:project).permit(:title, :description)
-    end
+  private
 
-    def find_project
-        @project = Project.find(params[:id])
-    end
+  def set_params
+    params.require(:project).permit(:title, :description)
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
 end
